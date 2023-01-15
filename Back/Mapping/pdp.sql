94c1545d53b7ca6cd336096b9a0d95b677845337
Brais Iglesias <91885206+BraisIglesias17@users.noreply.github.com>
2023-01-15 14:24:02 +0100

    Update sql para añadir crear BD y usuario

--- Back/Mapping/pdp.sql
+++ Back/Mapping/pdp.sql
@@ -1,26 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 15-01-2023 a las 11:22:57
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 7.4.33
+-- DAMOS PERMISO USO Y BORRAMOS EL USUARIO QUE QUEREMOS CREAR POR SI EXISTE
+--
+-- GRANT USAGE ON * . * TO `pdp`@`localhost`;
+-- DROP USER `pdp`@`localhost`;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Comentario
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `pdp`
+-- CREAMOS EL USUARIO Y LE DAMOS PASSWORD,DAMOS PERMISO DE USO Y DAMOS PERMISOS SOBRE LA BASE DE DATOS.
 --
-
+CREATE USER IF NOT EXISTS `pdp`@`localhost` IDENTIFIED BY 'pdp';
+/*GRANT USAGE ON *.* TO `pdp`@`localhost` REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;*/
+/*GRANT ALL PRIVILEGES ON `pdp`.* TO `pdp`@`localhost` WITH GRANT OPTION;*/
+GRANT ALL ON *.* TO 'pdp'@'localhost';
+FLUSH PRIVILEGES;
+/*CREACIÓN DE TABLAS*/
+DROP DATABASE IF EXISTS `pdp`;
+CREATE DATABASE `pdp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+--
+-- SELECCIONAMOS PARA USAR
+--
+USE `pdp`;
 -- --------------------------------------------------------
 
 --
